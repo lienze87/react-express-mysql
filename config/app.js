512c19606f9aee29ca48a6739b66d6dd8019ba28
李恩泽 <lienze87@gmail.com>
2020-07-24 11:12:28 +0800

    Create app.js

---
+++ config/app.js
@@ -0,0 +1,16 @@
+module.exports={	
+  mysql:{	
+    host:'localhost',	
+    user:'root',	
+    password:'23456789',	
+    database:'ezdb',	
+    port:3306	
+  },	
+  server:{	
+    url:"http://loaclhost:8000",	
+    host:"localhost",	
+    port:8000	
+  },	
+  pageStep:5,	
+  cookieName:'azeBlog',	
+} 
