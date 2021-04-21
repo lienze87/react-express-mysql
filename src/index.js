9ae9626dbf9b27b89e7c9a103461feab68205e65
lienze <lienze87@gmail.com>
2021-04-21 21:21:06 +0800

    初始化项目

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router,Route,Switch } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './Store';
-import './reset.css';
-import Home from './containers/Home';
-import Admin from './containers/Admin';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-
-console.log(store.getState());
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <Switch>
-        <Route  path="/admin" component={Admin}/>  
-        <Route  component={Home}/>
-      </Switch>
-    </Router>
-  </Provider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
