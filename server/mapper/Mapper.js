6768cfb7ccaaff993e3706681cc85a9f952a6da4
lienze <lienze87@gmail.com>
2021-04-28 22:54:06 +0800

    修改sql语句

--- server/mapper/Mapper.js
+++ server/mapper/Mapper.js
@@ -1,29 +1,51 @@
 module.exports = {
   //所有sql语句均使用占位符，较为灵活且可以避免SQL注入
   article: {
-    insert: `insert into articles (pid,author,date_utc,date_modify,title,detail,detail_md,excerpt,category,
-      link_num,status,comment_status,comment_count,filter_key) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?)`,
+    create: `
+      CREATE TABLE IF NOT EXISTS article(
+        id INT UNSIGNED AUTO_INCREMENT,
+        pid INT UNSIGNED, 
+        author VARCHAR( 255 ) NOT NULL,
+        title VARCHAR( 255 ) NOT NULL,
+        detail TEXT NOT NULL,
+        detail_md MEDIUMTEXT NOT NULL,
+        date_utc DATETIME,
+        date_modify DATETIME,
+        filter_key VARCHAR( 255 ) NOT NULL,
+        status ENUM( '0', '1' ) NOT NULL DEFAULT '1', 
+        PRIMARY KEY ( id )
+    )`,
+    insert: `insert into articles (pid,author,date_utc,date_modify,title,detail,detail_md,
+      status,filter_key) values ( ?, ?, ?, ?, ?, ?, ?, ?, ? )`,
     update: "update articles set ? where pid=?",
-    delete: `update articles set status='close' where pid=?`,
+    delete: `update articles set status='0' where pid=?`,
     queryById:
-      "select pid,author,date_utc,title,detail_md,category,comment_count from articles where pid=?",
-    queryByType: `select pid,author,date_utc,title,excerpt,category,comment_count from articles where 
-                category=? and status='open' order by id DESC limit ?,?`,
-    queryAll: `select pid,author,date_utc,title,excerpt,category,comment_count from articles where 
-              status='open' order by id DESC limit ?,?`,
-    adminQueryList: `select pid,author,date_utc,title,category,comment_status,comment_count from articles where
-                    status='open' order by id DESC limit ?,?`,
-    queryHotList: `select pid,author,title from articles where status='open' order by read_count DESC limit 0,5`,
-    queryCount: `select COUNT(id) from articles where status='open'`,
-    updateReadCount: "update articles set read_count=? where pid=?",
-    updateCommentCount: "update articles set comment_count=? where pid=?",
+      "select pid,author,date_utc,title,detail_md from articles where pid=?",
+    queryAll: `select pid,author,date_utc,title from articles where 
+              status='1' order by id DESC limit ?, ?`,
+    adminQueryList: `select pid,author,date_utc,title from articles where
+                    status='1' order by id DESC limit ?, ?`,
+    queryCount: `select COUNT( id ) from articles where status='1'`,
   },
   user: {
+    create: `
+      CREATE TABLE IF NOT EXISTS user(
+        id INT UNSIGNED AUTO_INCREMENT,
+        role ENUM( '1', '2' ) NOT NULL DEFAULT '1', 
+        name VARCHAR( 255 ) NOT NULL,
+        password VARCHAR( 255 ) NOT NULL,
+        nickname VARCHAR( 255 ) NOT NULL,
+        email VARCHAR( 255 ) NOT NULL,
+        date_register DATETIME,
+        date_modify DATETIME,
+        status ENUM( '0', '1' ) NOT NULL DEFAULT '1', 
+        PRIMARY KEY ( id )
+    )`,
     insert:
-      "insert into users (role,name,password,nickname,email,date_register,date_modify) values(?,?,?,?,?,?,?)",
+      "insert into users (role,name,password,nickname,email,date_register,date_modify) values(?, ?, ?, ?, ?, ?, ?)",
     update: "update users set ? where id=?",
-    delete: `update users set status='close' where id=?`,
-    queryByName: `select id,role,name,password,nickname,email,date_register from users where status='open' and name=?`,
-    adminQueryList: `select id,role,name,password,nickname,email,date_register from users where status='open' order by id DESC limit ?,?`,
+    delete: `update users set status='0' where id=?`,
+    queryByName: `select id,role,name,password,nickname,email,date_register from users where status='1' and name=?`,
+    adminQueryList: `select id,role,name,password,nickname,email,date_register from users where status='1' order by id DESC limit ?, ?`,
   },
 };
